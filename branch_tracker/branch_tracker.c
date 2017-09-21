74d457f54dd42351ac6bfb856eec0fd35702cb97
Luke Craig <craigla@rose-hulman.edu>
2017-09-21 10:35:29 -0400

    updating option to list all

--- branch_tracker/branch_tracker.c
+++ branch_tracker/branch_tracker.c
@@ -22,7 +22,7 @@ int before_block_exec(CPUState *cpu, TranslationBlock *tb) {
 		panda_arg_list *args = panda_get_args("llvm_trace");
 		uint32_t pid = panda_parse_uint32(args, "PID", 0);
 		int cr3 = (int) panda_current_asid(cpu);
-		if (cr3 == pid || pid == -1){
+		if (cr3 == pid || pid == 1337){
 			CPUArchState *env = (CPUArchState*)cpu->env_ptr;
 			uint64_t count = rr_get_guest_instr_count();
 			int eax = (int)env->regs[R_EAX];
