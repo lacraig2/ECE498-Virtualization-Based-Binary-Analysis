9fa8b7dddb06a9dd0091ce729bb1af3e9dc6bcbe
Luke Craig <craigla@rose-hulman.edu>
2017-09-21 12:51:13 -0400

    Added network introspection

--- branch_tracker/branch_tracker.c
+++ branch_tracker/branch_tracker.c
@@ -13,6 +13,8 @@ PANDAENDCOMMENT */
 
 bool init_plugin(void *);
 void uninit_plugin(void *);
+int replay_net_transfer(CPUState *env, uint32_t type, uint64_t src_addr,
+                            uint64_t dest_addr, uint32_t num_bytes);
 
 int before_block_exec(CPUState *cpu, TranslationBlock *tb);
 
@@ -32,9 +34,17 @@ int before_block_exec(CPUState *cpu, TranslationBlock *tb) {
     return 0;
 }
 
+int replay_net_transfer(CPUState *env, uint32_t type, uint64_t src_addr,
+                            uint64_t dest_addr, uint32_t num_bytes){
+	printf("NET TRANSFER\n")
+	return 0;
+}
+
+
 bool init_plugin(void *self) {
-    panda_cb pcb = { .before_block_exec = before_block_exec };
+    panda_cb pcb = { .before_block_exec = before_block_exec, .replay_net_transfer=replay_net_transfer};
     panda_register_callback(self, PANDA_CB_BEFORE_BLOCK_EXEC, pcb);
+    panda_enable_precise_pc();
     return true;
 }
 
