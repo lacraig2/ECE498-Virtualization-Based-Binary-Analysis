500a5af252ba98e46dec4fbddb382eb950f54818
Luke Craig <craigla@rose-hulman.edu>
2017-09-21 10:19:00 -0400

    follow particular process

--- branch_tracker/branch_tracker.c
+++ branch_tracker/branch_tracker.c
@@ -17,12 +17,17 @@ void uninit_plugin(void *);
 int before_block_exec(CPUState *cpu, TranslationBlock *tb);
 
 int before_block_exec(CPUState *cpu, TranslationBlock *tb) {
+	panda_arg_list *args = panda_get_args("llvm_trace");
+	uint32_t pid = panda_parse_uint32(args, "PID", 0);
+	//char *base = panda_parse_string(args, "base", NULL);
 	#if defined (TARGET_I386)
-		CPUArchState *env = (CPUArchState*)cpu->env_ptr;
-		int cr3 = (int) panda_current_asid(cpu);
-		uint64_t count = rr_get_guest_instr_count();
-		int eax = (int)env->regs[R_EAX];
-		printf("INSTR: 0x%" PRIx64 " EAX: 0x%" PRIx32 " CR3: 0x%" PRIx32 "\n", count, eax, cr3);
+		if (cr3 == pid){
+			CPUArchState *env = (CPUArchState*)cpu->env_ptr;
+			int cr3 = (int) panda_current_asid(cpu);
+			uint64_t count = rr_get_guest_instr_count();
+			int eax = (int)env->regs[R_EAX];
+			printf("INSTR: 0x%" PRIx64 " EAX: 0x%" PRIx32 " CR3: 0x%" PRIx32 "\n", count, eax, cr3);
+		}
 	#endif
     return 0;
 }
