0e2445c72ded3f75913531f19490169b4f8e20e0
Luke Craig <craigla@rose-hulman.edu>
2017-10-09 20:39:43 -0400

    tried removing fs_pin.h

--- kernelinfo/mount.h
+++ kernelinfo/mount.h
@@ -2,7 +2,7 @@
 #include <linux/seq_file.h>
 #include <linux/poll.h>
 // #include <linux/ns_common.h>
-#include <linux/fs_pin.h>
+// #include <linux/fs_pin.h>
 
 struct mnt_namespace {
 	atomic_t		count;
