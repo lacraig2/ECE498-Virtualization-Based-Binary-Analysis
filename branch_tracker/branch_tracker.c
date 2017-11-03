ee0db2756bdb8784a7160a728135b1845b9f32a1
Luke Craig <craigla@rose-hulman.edu>
2017-11-03 12:23:33 -0400

    checking if in kernel

--- branch_tracker/branch_tracker.c
+++ branch_tracker/branch_tracker.c
@@ -43,11 +43,15 @@ int virt_mem_w(CPUState *cpu, target_ulong pc, target_ulong addr, target_ulong s
 
 int virt_mem_w(CPUState *cpu, target_ulong pc, target_ulong addr, target_ulong size, void *buf){
     #ifdef TARGET_I386
-    OsiProc *current = get_current_process(cpu);
+    if (!panda_in_kernel(cpu)){}
+        OsiProc *current = get_current_process(cpu);
     // printf("%s\n",current->name);
     // if (!strcmp("vuln", current->name)){
-    printf("%s write: %lu, %s\n", current->name, (uint64_t) addr, (char*) buf);
+        printf("%s write: %lu, %s\n", current->name, (uint64_t) addr, (char*) buf);
     // }
+    }else{
+        printf("kernel %s", (char*) buf);
+    }
     #endif
     return 0;
 }
