12c115aca716ff2d88fef4d0b3e18b8e699bca42
Luke Craig <craigla@rose-hulman.edu>
2017-11-29 17:08:45 -0500

    looking at ebp

--- kernel_stack_grabber/kernel_stack_grabber.c
+++ kernel_stack_grabber/kernel_stack_grabber.c
@@ -28,8 +28,9 @@ int before_block_exec(CPUState *env, TranslationBlock *tb) {
 	if (panda_in_kernel(env)){
 		uint64_t cr3 = (uint64_t) panda_current_asid(env);
 		uint64_t page_val = cr3 & 0b1111111111111111111111111111111111111111111111111110000000000000;
-		uint64_t esp = (uint64_t)((CPUArchState*)cpu->env_ptr)->regs[R_ESP];
-		printf("%"PRIx64" %"PRIx64"\n",cr3, page_val);
+		uint64_t esp = (uint64_t)((CPUArchState*)env->env_ptr)->regs[R_ESP];
+		uint64_t ebp = (uint64_t)((CPUArchState*)env->env_ptr)->regs[R_EBP];
+		printf("%"PRIx64" %"PRIx64" %"PRIx64"%"PRIx64"\n",cr3, page_val,esp,ebp);
 		// offset 1111111111111111111111111111111111111111111111111110000000000000
 	}
 	#endif
