1a9041e270ed99181bfb65a0f8dfddbd13a10c34
Luke Alexander Craig <craigla@rose-hulman.edu>
2018-01-13 02:21:09 -0500

    actually use my variables

--- kernel_stack_grabber/kernel_stack_grabber.c
+++ kernel_stack_grabber/kernel_stack_grabber.c
@@ -47,7 +47,7 @@ int before_block_exec(CPUState *env, TranslationBlock *tb) {
 
 				int task_struct_size = 8192;
   			unsigned char *t_struct_buf = (unsigned char *) malloc(size);
-  		  int err_t_struct = panda_virtual_memory_rw(env, int_buf[0], buf, size, 0);
+  		  int err_t_struct = panda_virtual_memory_rw(env, int_buf[0], buf, task_struct_size, 0);
 				if (err_t_struct==-1){
          	printf("couldn't read memory.\n");
          	return -1;
